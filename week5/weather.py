03a9179fad471e632418af671979ca8b954f0094
Trinity Washington <Trinity.Washington@bsu.edu>
2023-10-12 14:08:24 -0400

    week 7 Tuesday

--- week5/weather.py
+++ week5/weather.py
@@ -1,10 +1,23 @@
 import json
 import ssl
-from urllib.request import urlopen  #urlib means libaray 
+from urllib.request import urlopen
 
 def CAAlert():
-    pass
-    #url = add the website
-    #context = 
-    #repsonse = 
-    #weatherData = json.loads(response.read()) # contains all data from the website 
+    url = "https://api.weather.gov/alerts/active?area=CA"
+    context = ssl._create_unverified_context()
+    response = urlopen(url, context=context)
+    weatherData = json.loads(response.read())
+    return weatherData["features"][0]["properties"]["event"]
+
+def main():
+    state = input("Enter two-character state code: ")
+    url = "https://api.weather.gov/alerts/active?area=" + state
+    context = ssl._create_unverified_context()
+    response = urlopen(url, context=context)
+    weatherData = json.loads(response.read())
+    for e in weatherData["features"]:
+        print(e["properties"]["event"])
+        print(e["properties"]["headline"])
+        print(e["properties"]["description"])
+        
+main()
